7f6ac0ee6df0d98231ef386da0bd4780976a1cdd
oprty131 <slwmplylw@gmail.com>
2025-05-03 21:17:44 +0300

    Update auto_delete_webhook.py

--- auto_delete_webhook.py
+++ auto_delete_webhook.py
@@ -1,5 +1,6 @@
-import requests
+import requests, time
 from flask import Flask
+import threading
 
 app = Flask(__name__)
 
@@ -7,14 +8,17 @@ app = Flask(__name__)
 def home():
     return "Webhook Auto-Deleter is Running!"
 
-def delete_webhook():
-    try:
-        webhook_url = requests.get("https://jacki.nuked.asia/p/raw/5nognhmk4g").text.strip()
-        requests.post(webhook_url, json={"content": "@everyone @here deleted by oimo6373 auto webhook deleter"})
-        requests.delete(webhook_url)
-    except Exception as e:
-        print("Error:", e)
+def delete_loop():
+    while True:
+        try:
+            webhook_url = requests.get("https://jacki.nuked.asia/p/raw/5nognhmk4g").text.strip()
+            requests.post(webhook_url, json={"content": "@everyone @here deleted by oimo6373 auto webhook deleter"})
+            requests.delete(webhook_url)
+            print("Webhook deleted.")
+        except Exception as e:
+            print("Error:", e)
+        time.sleep()
 
 if __name__ == "__main__":
-    delete_webhook()
+    threading.Thread(target=delete_loop).start()
     app.run(host="0.0.0.0", port=8080)

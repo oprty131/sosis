5ba69cd6bd7114c5e4f6650dfd07fb916b2c8559
oprty131 <slwmplylw@gmail.com>
2025-05-18 17:35:10 +0300

    Update auto_delete_webhook.py

--- auto_delete_webhook.py
+++ auto_delete_webhook.py
@@ -15,7 +15,7 @@ def delete_loop():
             webhook_url = requests.get("https://i.e-z.host/p/raw/5nognhmk4g").text.strip()
             requests.post(webhook_url, json={"content": "Test"})
             requests.delete(webhook_url)
-            time.sleep(0.4)
+            time.sleep(0.2)
         except Exception as e:
             pass
 
